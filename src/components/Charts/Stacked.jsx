575e54815af78b20192a6a2c9ce8eb473eada45f
DansBody <djhoung@gmail.com>
2022-10-28 15:57:29 +0800

    1028

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,Legend,Catagory,StackingColumnSeries,Tooltip } from '@syncfusion/ej2/office-chart'
 
 const Stacked = () => {
   return (
